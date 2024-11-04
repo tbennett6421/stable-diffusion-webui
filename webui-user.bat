55e5ada58c56275cff374fa83277d4cbb7b4c8c0
Tyler Bennett <tbennett6421@users.noreply.github.com>
2024-11-03 19:05:25 -0600

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--xformers --api --listen
+set COMMANDLINE_ARGS=--xformers --api --listen --port xxxx --update-check --tls-keyfile xxxx --tls-certfile xxx --disable-tls-verify
 
 call webui.bat
